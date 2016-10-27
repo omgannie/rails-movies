67d1adfe73fe814b7ee9957df72187963fa20fe7
Keri Gannon <email": null>
2016-10-27 09:54:45 -0500

    Update routes to only include necessary routes

--- movies_app/config/routes.rb
+++ movies_app/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'movies#index'
 
-  resources :movies, :users, :sessions
+  resources :users, only: [:index, :new]
+  resources :sessions, only: [:index, :new, :destroy]
+  resources :movies, only: [:index, :show]
 
   root to: 'movies#index'
 end
