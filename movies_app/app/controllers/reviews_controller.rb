7f51bcc19d6388ed5a2b743a90269aef285da075
omgannie <annie_oh@icloud.com>
2016-10-27 11:00:19 -0500

    Implement Reviews show and create

--- movies_app/app/controllers/reviews_controller.rb
+++ movies_app/app/controllers/reviews_controller.rb
@@ -1,9 +1,24 @@
 class ReviewsController < ApplicationController
   def new
+    @movie = Movie.find(params[:id])
+    @review = Review.new
+  end
 
+  def create
+    @movie = Movie.find(params[:id])
+    @review = Review.create(review_params)
+    @review.reviewer_id = @user.id
+    @review.save
+
+    if @review.valid?
+      redirect_to '/movies/#{@movie.id}'
+    else
+      @errors = @review.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
-
+    #
   end
 end
