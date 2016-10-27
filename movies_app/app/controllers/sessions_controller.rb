4ab2f247321b21687327fe4b0d069274d0882eef
Keri Gannon <email": null>
2016-10-27 15:32:19 -0500

    Remove p statements

--- movies_app/app/controllers/sessions_controller.rb
+++ movies_app/app/controllers/sessions_controller.rb
@@ -4,8 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    p "!!!!!!!!!!!!!!!!!!!"
-    p params
 
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
