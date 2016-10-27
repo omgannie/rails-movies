55fc753b2c545ad3269a37a967291f8cc835b62f
Keri Gannon <email": null>
2016-10-27 14:39:21 -0500

    Write and pass RSpec tests for UserController post#create method

--- movies_app/spec/controllers/users_controller_spec.rb
+++ movies_app/spec/controllers/users_controller_spec.rb
@@ -35,6 +35,38 @@ describe UsersController do
         post :create, user: { username: user.username, email: user.email, password: user.password }
         expect(response).to redirect_to root_path
       end
+
+      it "updates trusted_reviewer to true if the correct key was passed" do
+        post :create, user: { username: user.username, email: user.email, password: user.password, key: "monkey" }
+        expect(assigns(:user).trusted_reviewer).to eq true
+      end
+
+      it "keeps trusted_reviewer to false if the correct key was not passed" do
+        post :create, user: { username: user.username, email: user.email, password: user.password }
+        expect(assigns(:user).trusted_reviewer).to eq false
+      end
+    end
+
+    context "when invalid params are passed" do
+      it "responds with a status code 200" do
+        post :create, user: { username: user.username, email: user.email }
+        expect(response).to have_http_status 200
+      end
+
+      it "does not create a new user in the database" do
+        post :create, user: { username: user.username, email: user.email }
+        expect(assigns(:user).save).to eq false
+      end
+
+      it "assigns the unsaved user as @user" do
+        post :create, user: { username: user.username, email: user.email }
+        expect(assigns(:user)).to be_a_new User
+      end
+
+      it "renders the :new template" do
+        post :create, user: { username: user.username, email: user.email }
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
